2b083ac261a961fcaebe9409054c8e334f0f5bcd
Lena Hou <lenahou2013@gmail.com>
2022-01-10 23:44:19 -0500

    commit tree c8c6788f6bf51cfcc168e130c52f7dd922a708cb

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Form from './components/Form'
+import ParentComponent from './components/ParentComponent';
+// import Form from './components/Form'
 
 ReactDOM.render(
-  <Form />,
+  <ParentComponent />,
   document.getElementById('root')
 );
